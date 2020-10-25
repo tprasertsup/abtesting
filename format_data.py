50247d911d1c7e6dc42744504c3a4496d339f130
tprasertsup <thananun.pra@gmail.com>
2020-10-25 09:21:35 +0700

    Edit data and code

--- format_data.py
+++ format_data.py
@@ -1,44 +1,62 @@
 import pandas as pd
 
-''' Time to completion
-'''
-# data = pd.read_csv("myfilteredlog-comma.csv")
 data = pd.read_csv("myfilteredlog_comma.csv")
-# rowA = data["A: Time to complete"].copy().dropna().reset_index(drop=True)
-# rowB = data["B: Time to complete"].copy().dropna().reset_index(drop=True)
-# time_complete = pd.concat([rowA, rowB], axis=1, ignore_index=True)
-# time_complete.columns = ['A', 'B']
-time_complete_a = []
-time_complete_b = []
-for i in range(len(data)):
-    if data['time to complete'][i] > 0:
-        if data['style'][i] == 'A':
-            time_complete_a.append(data['time to complete'][i])
-        elif data['style'][i] == 'B':
-            time_complete_b.append(data['time to complete'][i])
 
-''' Return rate
-'''
-visited_userid = set()
-visited_pageclick_userid = set()
-return_user = {}
-return_user['A'] = set()
-return_user['B'] = set()
 user = {}
 user['A'] = set()
 user['B'] = set()
+user_pageload = {}  # map userid to page load
+user_timecomplete = {}  # map userid to last interaction
+user_checkout = set()  # add userid to the set if they go to page C
+return_user = set()
+
 for i in range(len(data)):
     cur_user = data['user id'][i]
-    pageclick_userid = (data['page load'][i], cur_user)
-    if pageclick_userid not in visited_pageclick_userid:
-        visited_pageclick_userid.add(pageclick_userid)
-        style = data['style'][i]
-        if style != 'C':
-            if cur_user not in user[style]:
-                user[style].add(cur_user)
-            else:
-                return_user[style].add(cur_user)
+    # if first log of cur user
+    if cur_user not in user_pageload:
+        user_pageload[cur_user] = data['page load'][i]
+    # if user lands to the checkout page
+    elif data['style'][i] == 'C':
+        # 1. add checkout user
+        user_checkout.add(cur_user)
+        # 2. update time to complete
+        user_timecomplete[cur_user] = data['page load'][i]
+    # if user clicks items
+    elif data['item click'][i] > 0:
+        # 1. add user to user[style]
+        user[data['style'][i]].add(cur_user)
+        # 2. update time to complete
+        user_timecomplete[cur_user] = data['item click'][i]
+    # if user returns to the page
+    elif cur_user in user_checkout and user_pageload[cur_user] != data['page load'][i]:
+        # 1. add return user
+        return_user.add(cur_user)
+        # 2. update time to complete
+        user_timecomplete[cur_user] = data['page load'][i]
 
-# [return_rate, no_return_rate]
-return_rate_a = [len(return_user['A']), len(user['A']) - len(return_user['A'])]
-return_rate_b = [len(return_user['B']), len(user['B']) - len(return_user['B'])]
+time_complete_a = []
+return_user_a = 0
+for cur_user in user['A']:
+    time_complete_a.append(
+        user_timecomplete[cur_user] - user_pageload[cur_user])
+    if cur_user in return_user:
+        return_user_a += 1
+
+time_complete_b = []
+return_user_b = 0
+for cur_user in user['B']:
+    time_complete_b.append(
+        user_timecomplete[cur_user] - user_pageload[cur_user])
+    if cur_user in return_user:
+        return_user_b += 1
+
+num_user_a = len(user['A'])
+num_user_b = len(user['B'])
+print("Number of users visiting version A:", num_user_a)
+print("Number of users visiting version B:", num_user_b)
+print("Number of return, no return user A: ",
+      return_user_a, num_user_a - return_user_a)
+print("Number of return, no return user B: ",
+      return_user_b, num_user_b - return_user_b)
+return_rate_a = [return_user_a, num_user_a - return_user_a]
+return_rate_b = [return_user_b, num_user_b - return_user_b]
--- get_stat.py
+++ get_stat.py
@@ -4,6 +4,8 @@ from abtesting import *
 print("Time to complete")
 print("avg time to completion of A: ", get_avg(time_complete_a))
 print("avg time to completion of B: ", get_avg(time_complete_b))
+print("stddev time to completion of A: ", get_stdev(time_complete_a))
+print("stddev time to completion of B: ", get_stdev(time_complete_b))
 print("t-score: ", get_t_score(time_complete_a, time_complete_b))
 print("t-test: ", perform_2_sample_t_test(time_complete_a, time_complete_b))
 
@@ -15,23 +17,26 @@ print("chi2: ", chi2_value(observed_grid))
 print("chi2 homogeneity test: ", perform_chi2_homogeneity_test(observed_grid))
 
 '''
+Number of users visiting version A: 26
+Number of users visiting version B: 18
+Number of return, no return user A:  19 7
+Number of return, no return user B:  13 5
+
 Time to complete
-avg time to completion of A:  22382.411003236244
-avg time to completion of B:  18007.76371308017
-t-score:  -3.262761000839354
-t-test:  0.0005964006277157073
-p-value < 0.05 -> statistically significant 
-The users use less time to complete in the design B.
+avg time to completion of A:  89055.84615384616
+avg time to completion of B:  106769.11111111111
+stddev time to completion of A:  70087.99687803458
+stddev time to completion of B:  88059.36463270866
+t-score:  -0.7115318865923278
+t-test:  0.24103753276363715
 
 Return rate
-A (return, no return):  [19, 13]
-B (return, no return):  [13, 10]
-chi2:  0.044776036743856204
-chi2 homogeneity test:  0.8324163867970478
-p-value > 0.05 -> statistically insignificant 
-There is no difference in return rate between design A and B.
+A (return, no return):  [19, 7]
+B (return, no return):  [13, 5]
+chi2:  0.003917378917378914
+chi2 homogeneity test:  0.9500938144413764
 
 Limitations
-Small samples
+Small samples, so a few edge cases have a lot of impact
 Different interest in catci
 '''
